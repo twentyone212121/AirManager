3d424216e33f098c1b89ce9920da357f8153e07b
alinapavlyuk <alino4kap12@gmail.com>
2023-12-10 02:57:46 +0900

    Create v.1

--- Sources/App/Controllers/manager_controller.swift
+++ Sources/App/Controllers/manager_controller.swift
@@ -5,7 +5,7 @@ final class ManagerController: RouteCollection {
         let manager_routes = routes.grouped("manager")
         manager_routes.get(use: indexHandler)
         manager_routes.post("action", use: actionHandler)
-        manager_routes.post("create", use: actionHandler)
+        manager_routes.post("create", use: createHandler)
     }
 
     func indexHandler(_ req: Request) throws -> EventLoopFuture<View> {
@@ -33,5 +33,20 @@ final class ManagerController: RouteCollection {
         }
 
         return req.view.render("DataTemplates/ManagerActions/\(templateName)")
+    }
+
+     func createHandler(_ req: Request) throws -> EventLoopFuture<View> {
+        guard let flight = try? req.content.decode(Flight.self) else {
+            print(req.content)
+            throw Abort(.badRequest)
+        }
+
+        print(flight)
+        do {
+            try req.application.databaseManager.addFlight(flight: flight)
+        } catch {
+            throw Abort(.badRequest)
+        }
+        return req.view.render("DataTemplates/confirmation", ["email": "beba"])
     }
 }
